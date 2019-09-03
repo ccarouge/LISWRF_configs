beedd31ada95d9fbc0b2b752d45c2ff47c0956be
C. Carouge <c.carouge@unsw.edu.au>
2019-09-03 13:53:21 +1000

    test_lis_v9p2 config

---
+++ tests_lis_v9p2/decks/run_LDTprelis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Run LDT
+#PBS -m ae
+#PBS -M c.carouge\@unsw.edu.au
+#PBS -P w35
+#PBS -l walltime=2:20:00
+#PBS -l mem=10GB
+#PBS -l ncpus=1
+#PBS -j oe
+#PBS -q express
+#PBS -l wd
+#PBS -l other=gdata1a
+
+codepath=/short/w35/ccc561/WRF/nuwrf/installs/newbuild/ldt/make
+if [ -x ${codepath}"/LDT" ]; then
+    export PATH="${codepath}:${PATH}"
+else
+    echo "ERROR: LDT not found"
+    exit 1
+fi
+
+module purge
+module load dot pbs
+module load openmpi/1.6
+module load netcdf/4.3.3.1
+
+
+# Run LIS
+LDT ldt.config.prelis
+
+cp LIS_offline/lis_input* /g/data1/w35/ccc561/LIS_WRF/tests_lis_v9p2/bdy_data
+
